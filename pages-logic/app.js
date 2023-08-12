5b1fbe07884ef79a656656a117170d7ecd819522
Angel Vera <angellvs71@gmail.com>
2023-08-12 19:11:04 -0400

    Penultimas versiones, nueva funcionalidad agregada

--- pages-logic/app.js
+++ pages-logic/app.js
@@ -4,10 +4,20 @@ let introductionExplanations = [1,2,3,4,5],
     explainationContainer = document.querySelector('.header_content__txt_vignets'),
     explainationSelectionMenu = document.querySelector('.header_content__stage_dotts'),
     explainationOption = document.querySelector('.header_content__stage_dotts__dott'),
-    optionButtonCopy = undefined
+    optionButtonCopy
 
 for (let index = 1; index < introductionExplanations.length; index++) {
         optionButtonCopy = explainationOption.cloneNode(true)
         optionButtonCopy.id = index
         explainationSelectionMenu.appendChild(optionButtonCopy)
 }
+
+const allVignetButtons = document.querySelector(".header_content__stage_dotts_dott")
+
+document.addEventListener('click', (e) => {
+    if (e.target.matches(".header_content__stage_dotts__dott")) {
+        explainationContainer.textContent = introductionExplanations[e.target.id]
+    }
+})
+
+const changeSlide = () => {}
