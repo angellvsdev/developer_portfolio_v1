b4a063c68f9e07d1636cbcd068eb96a4de299e4e
Angel Vera <angellvs71@gmail.com>
2023-08-16 02:35:44 -0400

    Se agregaron nuevas funcionalidades y código mediante JavaScript.

--- pages-logic/app.js
+++ pages-logic/app.js
@@ -1,4 +1,5 @@
-//import developerProfile from "./app_about-me";
+import { webProjectsList } from "./app_about-me.js";
+import { developerProfile } from "./app_about-me.js";
 
 let introductionExplanations = [1,2,3,4,5],
     explainationContainer = document.querySelector('.header_content__txt_vignets'),
@@ -14,10 +15,62 @@ for (let index = 1; index < introductionExplanations.length; index++) {
 
 const allVignetButtons = document.querySelector(".header_content__stage_dotts_dott")
 
+let intervalIndex = 0
+
 document.addEventListener('click', (e) => {
     if (e.target.matches(".header_content__stage_dotts__dott")) {
+        intervalIndex = e.target.id
         explainationContainer.textContent = introductionExplanations[e.target.id]
     }
 })
 
-const changeSlide = () => {}
+setInterval(() => {
+    if (intervalIndex < introductionExplanations.length) {
+        let stageOptions = document.querySelectorAll('.header_content__stage_dotts__dott')
+        let pastOption = intervalIndex - 1
+
+        stageOptions[stageOptions.length - 1].style.background = 'none'
+        stageOptions[intervalIndex].style.background = '#110800'
+
+        explainationContainer.textContent = introductionExplanations[intervalIndex]
+
+        intervalIndex++
+
+        stageOptions[pastOption].style.background = 'none'
+    }
+    else {
+        intervalIndex = 0
+    }
+}, 2000);
+
+console.log(webProjectsList)
+
+let stellarProjectContainer = {
+    projectFace: document.querySelector(".container__photo"),
+    projectName: document.querySelector(".project_data__name"),
+    shortProjectDescription: document.querySelector(".project_data__description"),
+    projectHref: document.querySelector('.project_data__view_more_opt'),
+    projectTechStack: document.querySelector(".project_data__used_tech_stack")
+}
+
+const showBestProject = () => {
+    webProjectsList.forEach(project => {
+        if (project.isBestProject) {
+            stellarProjectContainer.projectFace.src = project.footage[0]
+            stellarProjectContainer.projectName.textContent = project.titleName
+            stellarProjectContainer.shortProjectDescription.textContent = project.infoDescription
+            stellarProjectContainer.projectHref.href = project.infoModule
+            
+            let techInStack = document.createElement('li'), techNodeCopy
+            techInStack.classList.add('used_tech_stack__icon_el')
+
+            project.techsUsed.forEach(tech => {
+                techNodeCopy = techInStack.cloneNode()
+                techNodeCopy.innerHTML = developerProfile.technicalSkills.get(tech)
+                stellarProjectContainer.projectTechStack.appendChild(techNodeCopy)
+            })
+        }
+    })
+}
+
+showBestProject()
--- pages-logic/app_about-me.js
+++ pages-logic/app_about-me.js
@@ -8,3 +8,39 @@ export const developerProfile = {
     technicalSkills: new Map(),
     socialMediaLinks: []
 }
+
+developerProfile.technicalSkills.set('HTML', '<i class="fa-brands fa-html5"></i>')
+developerProfile.technicalSkills.set('CSS', '<i class="fa-brands fa-css3-alt"></i>')
+developerProfile.technicalSkills.set('JavaScript', '<i class="fa-brands fa-js"></i>')
+developerProfile.technicalSkills.set('CMD', '<i class="fa-solid fa-terminal"></i>')
+developerProfile.technicalSkills.set('Git', '<i class="fa-brands fa-git-alt"></i>')
+developerProfile.technicalSkills.set('GitHub', '<i class="fa-brands fa-github"></i>')
+developerProfile.technicalSkills.set('SASS', '<i class="fa-brands fa-sass"></i>')
+developerProfile.technicalSkills.set('React JS', '<i class="fa-brands fa-react"></i>')
+
+class webProject {
+    constructor(footage, titleName, infoDescription, techsUsed, completionTime, projectRepository, projectHyperlink, isBestProject, infoModule) {
+        this.footage = footage
+        this.titleName = titleName
+        this.infoDescription = infoDescription
+        this.techsUsed = techsUsed
+        this.completionTime = completionTime
+        this.projectRepository = projectRepository
+        this.projectHyperlink = projectHyperlink
+        this.isBestProject = isBestProject
+        this.infoModule = infoModule
+    }
+}
+
+const PROJECT_ONE = new webProject(
+    ['https://t0.gstatic.com/licensed-image?q=tbn:ANd9GcQkrjYxSfSHeCEA7hkPy8e2JphDsfFHZVKqx-3t37E4XKr-AT7DML8IwtwY0TnZsUcQ'],
+    'Dummie',
+    'Este proyecto va de un perrito muy bonito que me conseguí ayer, miren lo lindo que está nomas.',
+    ['HTML', 'CSS', 'JavaScript'],
+    'Infinite months',
+    'https://youtube.com',
+    'https://youtube.com',
+    true
+    )
+
+export let webProjectsList = [PROJECT_ONE]
